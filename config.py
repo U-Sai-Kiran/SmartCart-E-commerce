0e57e3d18c14c898f9dff3a3d4aabb1bcdef04b5
Sai Kiran <usirikayalasaikiran@gmail.com>
2026-08-12 14:53:17 +0530

    Fix mail configuration

--- config.py
+++ config.py
@@ -14,8 +14,8 @@ DB_NAME = os.getenv("DB_NAME", "smartcart_db")
 MAIL_SERVER = "smtp.gmail.com"
 MAIL_PORT = 587
 MAIL_USE_TLS = True
-MAIL_USERNAME = os.getenv("MAIL_USERNAME")
-MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
+MAIL_USERNAME = "usirikayalasaikiran@gmail.com"
+MAIL_PASSWORD = "madw vmfl pvac bbjk"
 
 UPLOAD_FOLDER = "static/uploads/product_images"
 
