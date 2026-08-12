5f4557f888dda4180eb0325fc5a7486bf6c7a68d
Sai Kiran <usirikayalasaikiran@gmail.com>
2026-08-12 15:14:55 +0530

    Fix Razorpay keys

--- config.py
+++ config.py
@@ -19,5 +19,5 @@ MAIL_PASSWORD = "madw vmfl pvac bbjk"
 
 UPLOAD_FOLDER = "static/uploads/product_images"
 
-RAZORPAY_KEY_ID = os.getenv("RAZORPAY_KEY_ID")
-RAZORPAY_KEY_SECRET = os.getenv("RAZORPAY_KEY_SECRET")
+RAZORPAY_KEY_ID = "rzp_test_TOPvLBZZZa2xDJ"
+RAZORPAY_KEY_SECRET = "KfboutVo1F873xqUamhnJgnS"
